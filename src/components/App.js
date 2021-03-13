a7184857be64c9aa42dd66074a768e785086b438
ni-wa <urban.hang274@gmail.com>
2021-03-13 18:24:23 +0900

    added firebase Auth with Google and E-mail

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from "react"
 
+import TestAuth from "./auth/TestAuth"
+
 
 const App = () => {
-  return(
-    <div>Dimbula</div>
+
+  return (
+    <React.Fragment>
+      <div>Dimbula Login</div>
+      <br />
+      <TestAuth />
+    </React.Fragment>
   )
 }
 export default App
