c7ed4fa85ea2d0e5f9166a14557536dc48f3b480
ni-wa <urban.hang274@gmail.com>
2021-04-05 23:52:15 +0900

    modified style

--- src/views/user/SignIn.js
+++ src/views/user/SignIn.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import { makeStyles } from '@material-ui/core/styles'
 import { 
+  Box,
   Container,
   Button,
   Divider,
@@ -19,15 +20,34 @@ const useStyles = makeStyles((theme) => ({
   signin_container:{
     display:'flex',
     flexFlow:'column',
+    border:'solid 2px #f0f0f0',
+    borderRadius:'16px',
+  },
+  title:{
+    fontSize:'32px',
+    textAlign:"center",
+    padding:"32px 0",
+    fontWeight:"bold",
+  },
+  sub_title:{
+    fontSize:'24px',
+    padding:"24px 0"
   },
   google_button:{
+    fontSize:"14px",
+    padding:"12px",
+    margin:"0 0 24px 0",
     '&:before':{
       backgroundImage:`url(${google_img})`,
       content:'',
       display:'inline-block',
       backgroundSize: 'contain',
       verticalAlign: 'middle',
-    }
+    },
+  },
+  google_logo:{
+    position:"absolute",
+    left:"16px"
   },
   signin_form:{
     display:"flex",
@@ -43,35 +63,40 @@ const SignIn = () => {
 
   return(
     <React.Fragment>
+        <h1 className={classes.title} >Dimbula</h1>
       <Container className={classes.signin_container} maxWidth="sm" >
-        <h1>Dimbula</h1>
-        <h2>ログイン</h2>
-        <Button 
-          variant="outlined"
-          className={classes.google_button}
-        >Googleでログイン
-        </Button>
-
-        <Divider variant="middle" />
-
-        <form className={classes.signin_form} autoComplete="off">
-          <TextField required name="mail" label="Mail Address" variant="outlined" />
-          <TextField required name="password" label="Password" type="password" autoComplete="off" variant="outlined" />
-          <Button variant="contained" color="secondary">
-            ログイン
+          <h2 className={classes.sub_title}>Sign In</h2>
+          <Button 
+            variant="outlined"
+            className={classes.google_button}
+          >
+            <img className={classes.google_logo} src={google_img} alt="Sign in with google" />
+            Sign In With Google
           </Button>
-        </form>
 
-        <FormControlLabel
-          control={<Checkbox color="primary" name="checked" />}
-          label="ログインしたままにする"
-          />
-        <span >パスワードをお忘れですか?</span>
+          <Box mb={5}>
+            <Divider variant="middle"/>
+          </Box>
 
-        <Divider variant="middle" />
+          <form className={classes.signin_form} autoComplete="off">
+            <TextField required name="mail" label="Mail Address" variant="outlined" />
+            <TextField required name="password" label="Password" type="password" autoComplete="off" variant="outlined" />
+            <Button variant="contained" color="primary">
+              Sign In
+            </Button>
+          </form>
 
-        <p>アカウントをお持ちではないですか?<span>  サインアップ</span></p>
+          <FormControlLabel
+            control={<Checkbox color="primary" name="checked" />}
+            label="Save login information"
+            />
+          <span >Forget your password?</span>
 
+          <Box mb={4}>
+            <Divider variant="middle" />
+          </Box>
+
+          <p>Don't have an account?<span>  Sign Up</span></p>
       </Container>
       
 
