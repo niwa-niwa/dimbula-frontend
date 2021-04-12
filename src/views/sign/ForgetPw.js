199cc935bff42961566dbe5a69de2c98384e1461
ni-wa <urban.hang274@gmail.com>
2021-04-12 21:32:27 +0900

    made validation

--- src/views/sign/ForgetPw.js
+++ src/views/sign/ForgetPw.js
@@ -1,4 +1,5 @@
-import React,{ useState } from "react"
+import React from "react"
+import { useForm, Controller } from 'react-hook-form'
 
 import { makeStyles } from '@material-ui/core/styles'
 import { 
@@ -17,18 +18,18 @@ import SignLink from './parts/SignLinks'
 import PATHS from '../../const/paths'
 
 
+const useStyles = makeStyles((theme) => ({ ...signStyle }))
+
 const ForgetPw = () => {
   const classes = useStyles()
-  const [pw, setPw] = useState('')
+  const { handleSubmit, control, formState:{errors}} = useForm()
 
-  const handleInputChange = (event) => {
-    setPw( event.target.value )
-  }
-
-  const submit = () => {
-    firebase.auth().sendPasswordResetEmail(pw).then(response => {
-      console.log(response)
-      // Todo display pop-up that say confirm email
+  const onSubmit = (data) => {
+    firebase.auth().sendPasswordResetEmail(
+      data.email
+      ).then(response => {
+        console.log(response)
+        // Todo display pop-up that say confirm email
       
     }).catch(e => {
       console.log(e)
@@ -42,27 +43,43 @@ const ForgetPw = () => {
         <h2 className={classes.sub_title}>Forget Password</h2>
       </Box>
 
-      <Box className={classes.signin_form} autoComplete="off">
-          <TextField
-            required
-            fullWidth
-            name="email"
-            label="Mail Address"
-            variant="outlined"
-            margin="none"
-            defaultValue={pw}
-            onChange={handleInputChange}
-            />
-          <Box mt={2}>
-            <Button
+      <Box component="form" onSubmit={handleSubmit(onSubmit)} className={classes.signin_form} autoComplete="off">
+        <Controller
+          name="email"
+          control={control}
+          defaultValue=''
+          render={
+            ({field})=><TextField
+              {...field}
+              required
               fullWidth
-              variant="contained"
-              color="primary"
-              onClick={submit}
-              >
-              Reset Password
-            </Button>
-          </Box>
+              name="email"
+              label="Email Address"
+              variant="outlined"
+              margin="none"
+              error={Boolean(errors.email)}
+              helperText={errors.email && errors.email.message
+            }/>
+          }
+          rules={{
+            required:true,
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+              message: 'invalid email address'
+            }
+          }}
+        />
+
+        <Box mt={2}>
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            type="submit"
+            >
+            Reset Password
+          </Button>
+        </Box>
       </Box>
 
       <Box my={3}>
@@ -82,6 +99,3 @@ const ForgetPw = () => {
 
 }
 export default ForgetPw
-
-
-const useStyles = makeStyles((theme) => ({ ...signStyle }))
