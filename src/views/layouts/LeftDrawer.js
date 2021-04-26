fbef00d5beac1dceb3dd4dd359cd06330326cbd3
niwa-niwa <urban.hang274@gmail.com>
2021-04-26 22:14:18 +0900

    added asyncGetTaskFolders

--- src/views/layouts/LeftDrawer.js
+++ src/views/layouts/LeftDrawer.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import { useSelector } from "react-redux"
+import React,{ useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import clsx from "clsx"
-import { makeStyles } from "@material-ui/core/styles"
-import Drawer from "@material-ui/core/Drawer"
-import Toolbar from "@material-ui/core/Toolbar"
-import List from "@material-ui/core/List"
-import Divider from "@material-ui/core/Divider"
-import ListItem from "@material-ui/core/ListItem"
-import ListItemIcon from "@material-ui/core/ListItemIcon"
-import ListItemText from "@material-ui/core/ListItemText"
-import InboxIcon from "@material-ui/icons/MoveToInbox"
-import MailIcon from "@material-ui/icons/Mail"
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Drawer,
+  Toolbar,
+  List,
+  Divider,
+  IconButton,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+} from "@material-ui/core";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import StarsIcon from "@material-ui/icons/Stars";
+import TodayIcon from "@material-ui/icons/Today";
+import AllInboxIcon from "@material-ui/icons/AllInbox";
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 
+import { selectTaskFolders, asyncGetTaskFolders } from "../../slices/taskSlice";
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: "flex",
   },
   drawer: {
     width: drawerWidth,
@@ -29,30 +38,50 @@ const useStyles = makeStyles((theme) => ({
     width: drawerWidth,
   },
   drawerContainer: {
-    overflow: 'auto',
+    overflow: "auto",
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     marginLeft: -drawerWidth,
   },
   contentShift: {
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
     marginLeft: 0,
   },
+  subHeader:{
+    display: "flex",
+    justifyContent: "space-between",
+  }
+
 }));
 
+const default_menu = [
+  { text: "Inbox", icon: <InboxIcon /> },
+  { text: "Today", icon: <TodayIcon />},
+  { text: "Starred", icon: <StarsIcon /> },
+  { text: "All", icon: <AllInboxIcon /> },
+];
 
-export default function PersistentDrawer({children}) {
-  const isOpen_drawer = useSelector((state) => state.drawer.isOpen_drawer)
-  const classes = useStyles()
+export default function PersistentDrawer({ children }) {
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const isOpen_drawer = useSelector((state) => state.drawer.isOpen_drawer);
+  const task_folders = useSelector(selectTaskFolders);
+
+  useEffect(() => {
+    const effect = async () => {
+      await dispatch(asyncGetTaskFolders());
+    }
+    effect();
+  }, [dispatch]);
 
   return (
     <div className={classes.root}>
@@ -68,25 +97,46 @@ export default function PersistentDrawer({children}) {
         <Toolbar />
         <div className={classes.drawerContainer}>
           <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemText primary={text} />
+            {default_menu.map((menu) => (
+              <ListItem button key={menu.text}>
+                <ListItemIcon>{menu.icon}</ListItemIcon>
+                <ListItemText primary={menu.text} />
               </ListItem>
             ))}
           </List>
           <Divider />
-          <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
+
+          <List
+            subheader={
+              <ListSubheader
+                component="div"
+                id="nested-list-subheader"
+                className={classes.subHeader}
+              >
+                Task Folders
+                <IconButton aria-label="delete" size="small">
+                  <AddCircleOutlineIcon />
+                </IconButton>
+              </ListSubheader>
+            }
+          >
+            {task_folders.map((folder) => (
+              <ListItem button key={folder.id}>
+                <ListItemIcon>
+                  <AssignmentIcon />
+                </ListItemIcon>
+                <ListItemText primary={folder.name} />
               </ListItem>
             ))}
           </List>
         </div>
       </Drawer>
 
-      <main className={clsx(classes.content, {[classes.contentShift]:isOpen_drawer})}>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: isOpen_drawer,
+        })}
+      >
         <Toolbar />
         {children}
       </main>
