886ef9347a147ea4b0932486fc2a6ffe4af7eb9a
niwa-niwa <urban.hang274@gmail.com>
2021-05-18 23:27:28 +0900

    created user info page

--- src/views/settings/Settings.js
+++ src/views/settings/Settings.js
@@ -1,151 +1,94 @@
 import React from "react";
-import { useDispatch } from 'react-redux';
+import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
 import {
+  Container,
   Button,
-}from "@material-ui/core";
-import { asyncDeleteUser } from '../../slices/userSlice';
+  TableRow,
+  Table,
+  TableBody,
+  TableContainer,
+  TableCell,
+} from "@material-ui/core";
+import { selectUser, asyncDeleteUser } from "../../slices/userSlice";
+import SelectDialog from "../layouts/SelectDialog";
 
-import firebase from "firebase/app";
-import firebase_config from "../../apis/firebase";
-import "firebase/auth";
-import {
-  FirebaseAuthProvider,
-  FirebaseAuthConsumer,
-  IfFirebaseAuthed,
-  IfFirebaseAuthedAnd,
-} from "@react-firebase/auth";
+const MainContainer = styled(Container)`
+  position: relative;
+  height: 80vh;
+  padding-top: 32px;
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  font-size: 2em;
+  margin-bottom: 32px;
+`;
+
+const CustomCell = styled(TableCell)`
+  border: none;
+`;
+
+const DeleteButton = styled(Button)`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+`;
 
 const Settings = () => {
   const dispatch = useDispatch();
+  const { userInfo } = useSelector(selectUser);
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
+  const onDelete = () => {
+    dispatch(asyncDeleteUser());
+  };
 
   return (
-    <React.Fragment>
-      <h1>Settings Index Page</h1>
+    <MainContainer maxWidth="sm">
+      <Title>User Information</Title>
+      <TableContainer>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <CustomCell align="right">Name</CustomCell>
+              <CustomCell align="center">
+                {userInfo.name || "no name"}
+              </CustomCell>
+            </TableRow>
+            <TableRow>
+              <CustomCell align="right">E-mail</CustomCell>
+              <CustomCell align="center">
+                {userInfo.email || "no email"}
+              </CustomCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
 
-      <FirebaseAuthProvider {...firebase_config} firebase={firebase}>
-        <div>
-          {/* Google Sign in */}
-          <button
-            onClick={() => {
-              const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-              firebase.auth().signInWithPopup(googleAuthProvider);
-            }}
-          >
-            Sign In with Google
-          </button>
-
-          {/* anonymously */}
-          <button
-            data-testid="signin-anon"
-            onClick={() => {
-              firebase.auth().signInAnonymously();
-            }}
-          >
-            Sign In Anonymously
-          </button>
-
-          <br />
-          <br />
-
-          {/* E-Mail Sign up */}
-          <button
-            onClick={async () => {
-              const res = await firebase
-                .auth()
-                .createUserWithEmailAndPassword(
-                  process.env.REACT_APP_LOGIN_EMAIL,
-                  process.env.REACT_APP_LOGIN_PASSWORD
-                );
-              res.user.sendEmailVerification();
-              console.log(res);
-            }}
-          >
-            Sign Up with Mail
-          </button>
-
-          {/* E-Mail Sign in */}
-          <button
-            onClick={() => {
-              firebase
-                .auth()
-                .signInWithEmailAndPassword(
-                  process.env.REACT_APP_LOGIN_EMAIL,
-                  process.env.REACT_APP_LOGIN_PASSWORD
-                );
-            }}
-          >
-            Log In with Mail
-          </button>
-
-          <br />
-          <br />
-
-          {/* Sign out */}
-          <button
-            onClick={() => {
-              firebase.auth().signOut();
-            }}
-          >
-            Sign Out
-          </button>
-
-          <FirebaseAuthConsumer>
-            {({ isSignedIn, user, providerId }) => {
-              return (
-                <pre style={{ height: 300, overflow: "auto" }}>
-                  {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
-                </pre>
-              );
-            }}
-          </FirebaseAuthConsumer>
-
-          <div>
-            <IfFirebaseAuthed>
-              {() => {
-                // To confirm verified Email
-                const user = firebase.auth().currentUser;
-                if (user.emailVerified) {
-                  return <div>You are authenticated</div>;
-                } else {
-                  return (
-                    <div>
-                      <div style={{ color: "#ff0000" }}>
-                        You have to verify email
-                      </div>
-                      <button
-                        onClick={() => {
-                          user.sendEmailVerification();
-                        }}
-                      >
-                        Please Send Email Again
-                      </button>
-                    </div>
-                  );
-                }
-              }}
-            </IfFirebaseAuthed>
-
-            <IfFirebaseAuthedAnd
-              filter={({ providerId }) => providerId !== "anonymous"}
-            >
-              {({ providerId }) => {
-                return <div>You are authenticated with {providerId}</div>;
-              }}
-            </IfFirebaseAuthedAnd>
-          </div>
-        </div>
-      </FirebaseAuthProvider>
-
-      <Button
+      <DeleteButton
         color="secondary"
         variant="contained"
-        onClick={() => {
-          dispatch(asyncDeleteUser());
-        }}
+        onClick={() => setIsOpen(true)}
       >
         Delete Account.
-      </Button>
-    </React.Fragment>
+      </DeleteButton>
+
+      <SelectDialog
+        isOpen={isOpen}
+        onClose={() => onClose()}
+        title="Are You Sure ?"
+        subtitle="You are going to delete your account .
+        It will delete all your data permanently"
+        OnCallback={() => onDelete()}
+      />
+    </MainContainer>
   );
 };
 export default Settings;
