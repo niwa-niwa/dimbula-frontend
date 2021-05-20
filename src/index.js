ccc699b21573af3593e732c97c0d4a987f7d01c0
niwa-niwa <urban.hang274@gmail.com>
2021-05-20 22:46:47 +0900

    modified color of primary and secondary

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import {customTheme} from './views/styles/customTheme';
 
 import App from "./views/App";
+import { MuiThemeProvider } from "@material-ui/core";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <MuiThemeProvider theme={customTheme}>
+      <App />
+    </MuiThemeProvider>
   </Provider>,
   document.querySelector("#root")
 );
