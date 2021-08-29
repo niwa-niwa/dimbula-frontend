b416f941728d1edbacbcf7b894a4601b6275a367
niwa-niwa <urban.hang274@gmail.com>
2021-08-29 15:48:35 +0900

    all delete

--- src/const/actions.js
+++
@@ -1,8 +0,0 @@
-const ACTIONS = {
-  "TASK_FOLDERS_CREATE":"taskFolders/create",
-  "TASK_FOLDERS_EDIT":"taskFolders/edit",
-  "TASKS_CREATE":"tasks/create",
-  "TASKS_EDIT":"tasks/edit",
-
-};
-export default Object.freeze(ACTIONS);
